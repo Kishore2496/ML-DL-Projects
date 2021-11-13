687f1fed2f21a230b4b94b5b7449da83fcd89c88
Kishore2496 <47376745+Kishore2496@users.noreply.github.com>
2021-11-13 22:23:34 +0530

    Update neural_networks.py

--- Neural_Nets_From_Scratch/neural_networks.py
+++ Neural_Nets_From_Scratch/neural_networks.py
@@ -19,28 +19,17 @@ print("Matplotlib: ", matplotlib.__version__)
 inputs = [1.0, 2.0, 3.0, 2.5]
 
 #Modelling 3 neurons with 3 weights and bias
-weights1 = [0.2, 0.8, -0.5, 1]
-weights2 = [0.5, -0.91, 0.26, -0.5]
-weights3 = [-0.26, -0.27, 0.17, 0.87]
+weights = [[0.2, 0.8, -0.5, 1],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
 
-bias1 = 2
-bias2 = 3
-bias3 = 0.5
+biases = [2, 3, 0.5]
 
-output1 = 0
-output2 = 0
-output3 = 0
-
-for i in range(len(inputs)):
-  output1 += inputs[i] * weights1[i]
-  output2 += inputs[i] * weights2[i]
-  output3 += inputs[i] * weights3[i]
-
-output1 += bias1
-output2 += bias2
-output3 += bias3
-
-output = output1 + output2 + output3
-
-print([output1, output2, output3])
+layer_output = []
+for weight_list, single_bias in zip(weights, biases):
+  current_output = 0
+  for indiv_weights, n_input in zip(weight_list, inputs):
+    current_output += indiv_weights * n_input
+  current_output += single_bias
+  layer_output.append(current_output)
 
